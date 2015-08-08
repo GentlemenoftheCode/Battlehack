7cbcf7d5437489270b030285311ff943906c2d87
Roshin C <idroshin2010@gmail.com>
2015-08-08 16:12:57 -0400

    Added Models

---
+++ Demo/Models/Raffle.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models
+{
+    public class Raffle
+    {
+        [ScaffoldColumn(false)]
+        public int RaffleID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Raffle Name")]
+        public string RaffleName { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "Raffle Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Total Amount")]
+        public double? UnitPrice { get; set; }
+
+        public int? CharityID { get; set; }
+
+        public virtual Charity Charity { get; set; }
+    }
+}
