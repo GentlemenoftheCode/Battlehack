58926532ddafeba32bc69e185df47d88f21628aa
Roshin C <idroshin2010@gmail.com>
2015-08-08 12:40:38 -0400

    BrainTree

--- Demo/Roshin.aspx.cs
+++ Demo/Roshin.aspx.cs
@@ -4,22 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Braintree;
 namespace Demo
 {
     public partial class Roshin : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var gateway = new BraintreeGateway
+            {
+                Environment = Braintree.Environment.SANDBOX,
+                MerchantId = "78c2hsmmg73s6sdg",
+                PublicKey = "jsyrqbxq2fqv456k",
+                PrivateKey = "004b6691b796db322c57c71343ecf592"
+            };
 
         }
 
-        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
-        {
-            DateTime hello = Calendar1.SelectedDate;
-            Label1.Text = hello.DayOfWeek.ToString();
-            Label2.Text = hello.Date.ToString();
-        }
     }
 }
--- Demo/Roshin.aspx.designer.cs
+++ Demo/Roshin.aspx.designer.cs
@@ -11,32 +11,5 @@ namespace Demo {
     
     
     public partial class Roshin {
-        
-        /// <summary>
-        /// Calendar1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Calendar Calendar1;
-        
-        /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
-        /// Label2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }
