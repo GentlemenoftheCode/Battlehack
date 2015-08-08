f5c94bcd6d0ec14e94330de524c5e92891c6309d
jphall93 <jph4@sunyorange.edu>
2015-08-08 17:12:04 -0400

    made charity name and raised amount dynamic

--- Demo/Charity.aspx.designer.cs
+++ Demo/Charity.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace Demo {
     
     
     public partial class Charity {
+        
+        /// <summary>
+        /// TotalRaisedLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label TotalRaisedLabel;
+        
+        /// <summary>
+        /// CharityNameLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label CharityNameLabel;
     }
 }
